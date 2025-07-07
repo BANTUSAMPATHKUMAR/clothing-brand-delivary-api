fd432af1dd64322c27a59aa841c903c85cf01e04
BANTUSAMPATHKUMAR <142529425+BANTUSAMPATHKUMAR@users.noreply.github.com>
2025-07-07 23:12:50 +0530

    Intial Commit

---
+++ src/main/java/com/clothing_brand_delivary/ClothingBrandDelivaryApplication.java
@@ -0,0 +1,13 @@
+package com.clothing_brand_delivary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingBrandDelivaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingBrandDelivaryApplication.class, args);
+	}
+
+}
